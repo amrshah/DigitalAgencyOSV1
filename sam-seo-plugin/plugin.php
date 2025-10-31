4d77185a426d0f2a3bcf94f60f0616249fc5e6e4
amrshah <amr.shah@gmail.com>
2025-10-31 15:40:47 +0500

    Create plugin.php

---
+++ sam-seo-plugin/plugin.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Plugin Name: SAM SEO Plugin
+ * Plugin URI: https://github.com/amrshah/sam-seo-plugin
+ * Description: SEO Plugin for WP
+ * Version: 1.0.0
+ * Author: Amrshah
+ * Author URI: https://github.com/amrshah
+ * License: Private License
+ * Text Domain: sam-seo-plugin
+ * Requires at least: 6.0
+ * Requires PHP: 8.0
+ */
+
+namespace SAM_SEO_PLUGIN;
+
+// Exit if accessed directly
+if (!defined('ABSPATH')) {
+    exit;
+}
